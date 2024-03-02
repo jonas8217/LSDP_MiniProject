70911563998d0cc4a044a22ac94be4daac9b40e1
jonas8217 <jonas82173000@gmail.com>
2024-03-03 00:03:23 +0100

    cleaned up counter.py and readied it for multiple images

--- counter.py
+++ counter.py
@@ -1,75 +1,118 @@
 import cv2
-from matplotlib import pyplot as plt 
 import numpy as np
 
-img_name = "EB-02-660_0595_0482.JPG"
+directory = "Images for first miniproject/"
 
-img = cv2.imread(f"Images for first miniproject/{img_name}")
-assert img is not None, "Failed to load image."
+def load_image_name(file_name : str, path : str = directory):
+    img = cv2.imread(path+file_name)
+    assert img is not None, f"Failed to load image: {file_name}"
+    return img
+
+def load_image_num(num : str):
+    return load_image_name(f"EB-02-660_0595_{num}.JPG")
+
+def generate_mean_and_cov_from_mask(img, mask):
+    pixels = img.reshape((-1))
+    mask = mask.reshape((-1))
+    annotated_pixels = pixels[mask == 255].reshape((-1,3))
+
+    mean = np.average(annotated_pixels, axis=0)
+    cov = np.cov(annotated_pixels.transpose())
+
+    return mean,cov
+
+def get_max_possible_mahalanobis_distance(mean,cov):
+    HSV_max = [180,255,255]
+    max_dist = 0
+    test_img = np.zeros((8,3))
+    for i in range(1,8):
+        test_img[i] = np.multiply(HSV_max,[i%2,(i//2)%2,i//4])
+    return np.max(get_mahalanobis_image(test_img,mean,cov))
+
+def get_mahalanobis_image(img,mean,cov):
+    # Calculate the euclidean distance to the reference_color annotated color.
+    pixels = img.reshape((-1,3))
+    shape_pixels = pixels.shape
+    diff = pixels - np.repeat([mean], shape_pixels[0], axis=0)
+    inv_cov = np.linalg.inv(cov)
+    moddotproduct = diff * (diff @ inv_cov)
+    mahalanobis_dist = np.sum(moddotproduct, 
+        axis=1)
+    
+    mahalanobis_distance_image = np.reshape(
+        mahalanobis_dist, 
+        img.shape[:-1])
+    
+    return mahalanobis_distance_image
+
+def generate_mahalanobis_mask(img,mean,cov,max_dist):
+    mahalanobis_distance_image = get_mahalanobis_image(img,mean,cov)
+
+    # Norm the image by the maximum possible distance for consistancy across images
+    mahalanobis_distance_image_norm = mahalanobis_distance_image / max_dist
+    
+    annotated_mahalanobis_threshold = np.zeros(mahalanobis_distance_image.shape,dtype=img.dtype)
+    annotated_mahalanobis_threshold[mahalanobis_distance_image_norm < 0.01] = 1
+    
+    mahalanobis_mask = np.reshape(
+        annotated_mahalanobis_threshold, 
+        (img.shape[0],
+         img.shape[1]))
+    
+    return mahalanobis_mask
 
 
-# create cropped image
+num = 482
+num = str(num).rjust(4,'0')
+num2 = 52
+num2 = str(num2).rjust(4,'0')
+
+img = load_image_num(num)
+mask = load_image_name(f"{num}_sliced_mask.JPG","masks/")
+
+# create cropped image (bottom right 1/8 by 1/8 of the image)
 H,W,_ = img.shape
-img2 = img[H-H//8:,W-W//8:]
-#cv2.imwrite('output/sliced.JPG', img2)
+img_sliced = img[H-H//8:,W-W//8:]
+cv2.imwrite(f'output/{num}_sliced.JPG', img_sliced)
 
-pumkin_mask = cv2.imread(f"output/sliced_mask2.JPG")
-assert pumkin_mask is not None, "Failed to load image."
-pumkin_mask = pumkin_mask.reshape((-1))
+img_sliced = cv2.cvtColor(img_sliced, cv2.COLOR_BGR2HSV)
 
-img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
+mean,cov = generate_mean_and_cov_from_mask(img_sliced, mask)
 
-pixels = img2.reshape((-1))
+print(mean)
+print(cov)
+max_dist_mahalanobis = get_max_possible_mahalanobis_distance(mean,cov)
 
-annotated_pixels = pixels[pumkin_mask == 255].reshape((-1,3))
+mahalanobis_mask = generate_mahalanobis_mask(img_sliced,mean,cov,max_dist_mahalanobis)
 
-avg = np.average(annotated_pixels, axis=0)
-cov = np.cov(annotated_pixels.transpose())
+annotated_image = img_sliced.copy()
+annotated_image[mahalanobis_mask == 0] = 0
+annotated_image_inv = img_sliced.copy()
+annotated_image_inv[mahalanobis_mask == 1] = 0
 
-print(avg,cov)
-cov = cov / 10
-# Calculate the euclidean distance to the reference_color annotated color.
-pixels = img2.reshape((-1,3))
-shape = pixels.shape
-diff = pixels - np.repeat([avg], shape[0], axis=0)
-inv_cov = np.linalg.inv(cov)
-moddotproduct = diff * (diff @ inv_cov)
-mahalanobis_dist = np.sum(moddotproduct, 
-    axis=1)
-
-print(f"{mahalanobis_dist.shape=}")
-
-mahalanobis_distance_image = np.reshape(
-    mahalanobis_dist, 
-    (img2.shape[0],
-        img2.shape[1]))
+annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_HSV2BGR)
+cv2.imwrite(f"output/{num}_annotated.JPG",
+        annotated_image)
+annotated_image_inv = cv2.cvtColor(annotated_image_inv, cv2.COLOR_HSV2BGR)
+cv2.imwrite(f"output/{num}_annotated_inv.JPG",
+        annotated_image_inv)
 
 
 
-print(mahalanobis_distance_image.shape)
+contours, hierarchy = cv2.findContours(mahalanobis_mask,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
+print(hierarchy[0][0:4])
+for i,h in enumerate(hierarchy[0]):
+    if h[2] != -1 or h[3] != -1:
+        print(h,contours[i][0])
 
-# Scale the distance image and export it.
-mahalanobis_distance_image_remapped = 255 * mahalanobis_distance_image / np.max(mahalanobis_distance_image)
-cv2.imwrite("output/mahalanobis.JPG",
-        255 - mahalanobis_distance_image_remapped)
-
-annotated_mahalanobis_threshold = img2.copy()
-annotated_mahalanobis_threshold[mahalanobis_distance_image_remapped > 30] = 0
-
-print(f"{annotated_mahalanobis_threshold.shape=}")
-
-annotated_mahalanobis_threshold = np.reshape(
-    annotated_mahalanobis_threshold, 
-    img2.shape)
-
-# annotated_mahalanobis_threshold = cv2.cvtColor(annotated_mahalanobis_threshold, cv2.COLOR_HSV2BGR)
-# cv2.imwrite("output/mahalanobis_threshold.JPG",
-#         annotated_mahalanobis_threshold)
+# h[0] and h[1] not useful
+# h[2] != -1 means it is there is another contour within it
+# h[3] != -1 means it is a contour within another contour
 
 # define
 min_pixels = 15 # TODO base it on the GSD and physical pumpkin size
 
-# annotated_mahalanobis_threshold = cv2.cvtColor(annotated_mahalanobis_threshold, cv2.COLOR_HSV2BGR)
-# res1,res2 = cv2.findContours(annotated_mahalanobis_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# TODO use standard cv2 functions to determine the area and circularity of the areas for classifying if it is in fact a pumpkin
+# maybe use a clustering alorithm to get more circular result of multiple pumpkins with overlapping regions
 
-# print(res1,res2)
+# maybe just count pixels and find out how many pixels per average pumpkin??
