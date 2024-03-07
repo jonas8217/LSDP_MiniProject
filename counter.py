6ceccc446b5be9f8932863d2322e47983c1ae736
jonas8217 <jonas82173000@gmail.com>
2024-03-07 21:27:06 +0100

    things work now :) tm

--- counter.py
+++ counter.py
@@ -89,10 +89,10 @@ def generate_onetime_mahalanobis_hsv_values():
 
 def generate_onetime_CIELAB_values():
     # cielab range ((0,-127,-127),(100,128,128))
-    img = load_image_name("09_blurred.JPG","output/09/")
+    img = load_image_name("window_0.jpg","rasterized/")
     img = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
     pixels = img.reshape((-1))
-    mask = load_image_name("window_9_blurred_mask.JPG","masks/")
+    mask = load_image_name("window_0_better_res_mask.JPG","masks/")
     mask = mask.reshape((-1))
     annotated_pixels = pixels[mask == 255].reshape((-1,3))
     range = np.array([np.min(annotated_pixels,axis=0),np.max(annotated_pixels,axis=0)])
@@ -129,6 +129,9 @@ cov = np.array([[   1.45837838,    6.33243243,   -3.57081081],
 max_dist_mahalanobis = 35554.24373277427
 
 cielab_range = generate_onetime_CIELAB_values()
+cielab_range[0][0] = 170
+cielab_range[1][1] = 255
+cielab_range[1][2] = 255
 print(cielab_range)
 #cielab_range = np.array([[102, 132, 143],[226, 157, 190]])
 
@@ -164,22 +167,21 @@ if TEST:
 overlap_px,rows,cols = list(map(int,open("rasterized/rasta_meta.csv","r").read().split("\n")[1].split(",")))
 
 
-
 total_pumpkins = 0
 for num in range(rows*cols):
     col,row = num%cols,num//cols
     img = load_image_name(f"window_{num}.jpg","rasterized/")
-    W,H,_ = img.shape
+    H,W,_ = img.shape
     
-    img_blurred = cv2.GaussianBlur(img,(3,3),0,borderType=cv2.BORDER_REPLICATE)
+    #img_blurred = cv2.GaussianBlur(img,(3,3),0,borderType=cv2.BORDER_REPLICATE)
     
     if MAHALANOBIS:
-        mahalanobis_mask = generate_mahalanobis_mask(img_blurred,mean,cov,max_dist_mahalanobis)
-        #mahalanobis_mask_blurred = cv2.medianBlur(mahalanobis_mask,3)
-        mask = mahalanobis_mask
+        mask = generate_mahalanobis_mask(img,mean,cov,max_dist_mahalanobis)
+        mask = cv2.medianBlur(mask,3)
 
     elif CIELAB:
-        mask = generate_cielab_inrange_mask(img_blurred,cielab_range)
+        mask = generate_cielab_inrange_mask(img,cielab_range)
+        mask = cv2.medianBlur(mask,3)
         
 
     if DEBUG:
@@ -187,9 +189,9 @@ for num in range(rows*cols):
         folder = f"output/{num_str}"
         if not os.path.exists(folder):
             os.mkdir(folder)
-        annotated_image = img_blurred.copy()
+        annotated_image = img.copy()
         annotated_image[mask == 0] = 0
-        annotated_image_inv = img_blurred.copy()
+        annotated_image_inv = img.copy()
         annotated_image_inv[mask != 0] = 0
         cv2.imwrite(f"{folder}/{num_str}_annotated.JPG",
                 annotated_image)
@@ -198,51 +200,109 @@ for num in range(rows*cols):
         cv2.imwrite(f"{folder}/{num_str}_mask.JPG",
                 mask)
         cv2.imwrite(f"{folder}/{num_str}_blurred.JPG",
-                img_blurred)
-        print(f"{folder}/{num_str}_blurred.JPG")
+                img)
 
     contours, hierarchy = cv2.findContours(mask,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
     hierarchy = hierarchy[0]
     #print(hierarchy[0][0:4])
 
-    average_pumpkin_area = 50
+    average_pumpkin_area = 64*4
+
+    small = 35
+    big = 235
+    cluster = 158
+    
+    
 
     pumpkins = 0
-
+    centers = []
+    print(num,row,col)
     for i,c,h in zip(list(range(len(contours))),contours,hierarchy):
         if h[3] == -1:
             area = cv2.contourArea(c)
-            if area < 5: # get rid of small areas
+            if area < 15: # get rid of small areas
                 continue
-            if h[2] != -1: # discount "holes" in patches of multiple pumpkins forming a ring
+            if h[2] != -1: # ignore "holes" in patches of multiple pumpkins forming a ring
                 inner_contours = get_sub_contours(i, contours, hierarchy)
                 for inner in inner_contours:
                     inner_area = cv2.contourArea(inner)
                     if inner_area > 5:
                         area -= inner_area
-            
             # check if the contour is touching the left or top edge, in which case ignore it, since it is "has been" counted in the overlap (unless first row or column)
+            test = False
             if col != 0:
                 if c[0,0][0] < overlap_px: # using for extra purpose, equivelent idea
+                    # print(c[0,0][0])
                     for coord in c:
                         if coord[0][0] == 0:
-                            continue
+                            test = True
+                            break
+            if test:
+                continue
             
+            test = False
             if row != 0:
                 if c[0,0][1] < overlap_px: # using for extra purpose, equivelent idea
                     for coord in c:
                         if coord[0][1] == 0:
-                            continue
+                            test = True
+                            break
+            if test:
+                continue
             
-            # now count all other countours which are in the image including those that thouch the boundery on the right and bottom side of the overlap pixels and the original image  
+            # now dont count all countours which do not thouch the boundery on the right and bottom side of the original image, but which are otherwise outside the image
+            # cases
+            if col != cols-1 and row == rows-1: # case one (last row)
+                if c[0,0][0] > W - overlap_px:
+                    for coord in c:
+                        if coord[0][0] <= W - overlap_px + 1:
+                            break
+                    else:
+                        continue
             
-            # not really a good method :(
-            # is arbitrarely dependant on the range calibration which is faulty due to the color loss the creation of the orthomosaic has had
-            pumpkins += max(1,round(area/average_pumpkin_area))
+            if col == cols-1 and row != rows-1: # case two (last column)
+                if c[0,0][1] > H - overlap_px:
+                    for coord in c:
+                        if coord[0][1] <= H - overlap_px + 1:
+                            break
+                    else:
+                        continue
+            
+            if col != cols-1 and row != rows-1: # case three (everything else)
+                if c[0,0][0] > W - overlap_px or c[0,0][1] > H - overlap_px:
+                    for coord in c:
+                        if coord[0][0] <= W - overlap_px + 1 and coord[0][1] <= H - overlap_px + 1:
+                            break
+                    else:
+                        continue
+            
+            amount = max(1,round(area/cluster))
+            
+            if DEBUG:
+                m = list(map(int,list(np.rint(np.mean(np.reshape(c,(-1,2)),axis=0)))))
+                if amount == 1:
+                    centers.append((m,-1))
+                else:
+                    centers.append((m,amount))
+                    
+            # is arbitrarely dependant on the calibration of the defined color range
+            pumpkins += amount #max(1,round(area/average_pumpkin_area))
 
     total_pumpkins += pumpkins
-    print(pumpkins)
-    exit()
+    print(f"{num}: {pumpkins=}")
+    
+    if DEBUG:
+        circle_annotated_image = img.copy()
+        for center in centers:
+            m,c = center
+            if c == -1:
+                cv2.circle(circle_annotated_image,m,2,(255,0,0),thickness=cv2.FILLED)
+            else:
+                cv2.putText(circle_annotated_image,str(c),(m[0]-8,m[1]+6),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=0.7,color=(255,0,0))
+        cv2.line(circle_annotated_image,(0, H - overlap_px),(W - overlap_px, H - overlap_px),(0,0,0),2)
+        cv2.line(circle_annotated_image,(W - overlap_px, 0),(W - overlap_px, H - overlap_px),(0,0,0),2)
+        cv2.imwrite(f"{folder}/{num_str}_circles.JPG",
+                circle_annotated_image)
 
 print(total_pumpkins)
 
